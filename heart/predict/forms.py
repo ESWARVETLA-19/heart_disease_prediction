24be514f8e451bcc4cae4b070fa3fb18392ff6db
ESWARA RAO VETLA <112643496+ESWARVETLA-19@users.noreply.github.com>
2025-01-10 06:39:56 +0000

    added

---
+++ heart/predict/forms.py
@@ -0,0 +1,150 @@
+from django import forms
+
+class HeartDiseaseForm(forms.Form):
+    fullname = forms.CharField(
+        label="Full Name",
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your name',
+            'class': 'input',
+            'required': True,
+        })
+    )
+
+    age = forms.IntegerField(
+        label="AGE",
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter Your Age',
+            'class': 'input',
+            'required': True,
+        })
+    )
+
+    sex = forms.ChoiceField(
+        label="Sex",
+        choices=[('male', 'Male'), ('female', 'Female')],
+        widget=forms.RadioSelect(attrs={
+            'class': 'radio',
+            'required': True,
+        })
+    )
+
+    chest_pain = forms.ChoiceField(
+        label="Chest Pain Type",
+        choices=[
+            ('0', 'Typical Angina'),
+            ('1', 'Atypical Angina'),
+            ('2', 'Nonanginal Pain'),
+            ('3', 'Asymptomatic'),
+        ],
+        widget=forms.Select(attrs={
+            'class': 'select',
+            'required': True,
+        })
+    )
+
+    resting_bp = forms.FloatField(
+        label="Resting BP",
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter value',
+            'class': 'input',
+            'step': '0.01',
+            'required': True,
+        })
+    )
+
+    cholesterol = forms.FloatField(
+        label="Serum Cholesterol (mg/dl)",
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter value',
+            'class': 'input',
+            'step': '0.01',
+            'required': True,
+        })
+    )
+
+    fasting_blood_sugar = forms.BooleanField(
+        label="Fasting blood sugar > 120 mg/dl",
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'checkbox',
+        })
+    )
+
+    exercise_induced_angina = forms.BooleanField(
+        label="Exercise induced Angina",
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'checkbox',
+        })
+    )
+
+    ecg = forms.ChoiceField(
+        label="Resting Electrocardiograph Result",
+        choices=[
+            ('0', 'Normal'),
+            ('1', 'ST-T wave abnormality'),
+            ('2', 'Left ventricular hypertrophy'),
+        ],
+        widget=forms.Select(attrs={
+            'class': 'select',
+            'required': True,
+        })
+    )
+
+    max_heart_rate = forms.FloatField(
+        label="Maximum Heart Rate Achieved",
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter value',
+            'class': 'input',
+            'step': '0.01',
+            'required': True,
+        })
+    )
+
+    old_peak = forms.FloatField(
+        label="Old Peak",
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter value',
+            'class': 'input',
+            'step': '0.01',
+            'required': True,
+        })
+    )
+
+    slope = forms.ChoiceField(
+        label="Slope of the Peak Exercise ST Segment",
+        choices=[
+            ('0', 'Up Sloping'),
+            ('1', 'Flat'),
+            ('2', 'Down Sloping'),
+        ],
+        widget=forms.Select(attrs={
+            'class': 'select',
+            'required': True,
+        })
+    )
+
+    vessels = forms.IntegerField(
+        label="Number of Major Vessels (0-3)",
+        min_value=0,
+        max_value=3,
+        widget=forms.NumberInput(attrs={
+            'placeholder': 'Enter value',
+            'class': 'input',
+            'required': True,
+        })
+    )
+
+    thal = forms.ChoiceField(
+        label="Thal Value",
+        choices=[
+            ('0', 'Negative'),
+            ('1', 'Positive'),
+            ('2', 'In conclusive'),
+        ],
+        widget=forms.Select(attrs={
+            'class': 'select',
+            'required': True,
+        })
+    )
--- heart/predict/views.py
+++ heart/predict/views.py
@@ -1,42 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HeartDiseaseForm
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
+@csrf_exempt 
 def heart_disease_form(request):
     if request.method == 'POST':
-        fullname = request.POST.get('fullname')
-        age = request.POST.get('dob')
-        sex = request.POST.get('sex')
-        chest_pain = request.POST.get('chest-pain')
-        resting_bp = request.POST.get('resting-bp')
-        cholesterol = request.POST.get('cholesterol')
-        fasting_blood_sugar = 'fasting-blood-sugar' in request.POST
-        exercise_induced_angina = 'exercise-induced-angina' in request.POST
-        ecg = request.POST.get('ecg')
-        max_heart_rate = request.POST.get('max-heart-rate')
-        old_peak = request.POST.get('old-peak')
-        slope = request.POST.get('slope')
-        vessels = request.POST.get('vessels')
-        thal = request.POST.get('thal')
-        result_data = {
-            'fullname': fullname,
-            'age': age,
-            'sex': sex,
-            'chest_pain': chest_pain,
-            'resting_bp': resting_bp,
-            'cholesterol': cholesterol,
-            'fasting_blood_sugar': fasting_blood_sugar,
-            'exercise_induced_angina': exercise_induced_angina,
-            'ecg': ecg,
-            'max_heart_rate': max_heart_rate,
-            'old_peak': old_peak,
-            'slope': slope,
-            'vessels': vessels,
-            'thal': thal
-        }
+        form = HeartDiseaseForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            fullname = cleaned_data.get('fullname')
+            age = cleaned_data.get('dob')
+            sex = cleaned_data.get('sex')
+         
+            return render(request, 'result.html', {'cleaned_data': cleaned_data})
+    else:
+        form = HeartDiseaseForm()
 
-        # Render the result.html template with the result data
-        return render(request, 'result.html', {'result_data': result_data})
-    
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'form': form})
