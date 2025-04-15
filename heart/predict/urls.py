567dbb2b342258276b2f6678e955ffd4943d0f4b
ESWARA RAO VETLA <112643496+ESWARVETLA-19@users.noreply.github.com>
2025-04-15 07:27:59 +0000

    added pdf

--- heart/predict/urls.py
+++ heart/predict/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.heart_disease_form, name='heart_disease_form'),
+    path('generate_pdf/', views.generate_pdf, name='generate_pdf'),  
 ]
+ 
  
